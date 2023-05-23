bf4bb2c9188ff7dab82eb32d57d2574631574c47
namuaree <lolec2018@mail.ru>
2023-05-23 19:38:58 +0700

    Added test CorrectPoint2 in unittest.cpp

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -35,4 +35,32 @@ TEST(draw, CorrectPoint1)
     button.menuNumSet(0);
 }
 
+TEST(draw, CorrectPoint2)
+
+{
+    points.x = 40;
+    points.y = 280;
+    Mouse::setPosition(points, window);
+    ASSERT_EQ(2, button.draw(40, 180, 280, 380, window, true));
+    button.menuNumSet(0);
+
+    points.x = 86;
+    points.y = 280;
+    Mouse::setPosition(points, window);
+    ASSERT_EQ(2, button.draw(40, 180, 280, 380, window, true));
+    button.menuNumSet(0);
+
+    points.x = 40;
+    points.y = 326;
+    Mouse::setPosition(points, window);
+    ASSERT_EQ(2, button.draw(40, 180, 280, 380, window, true));
+    button.menuNumSet(0);
+
+    points.x = 86;
+    points.y = 326;
+    Mouse::setPosition(points, window);
+    ASSERT_EQ(2, button.draw(40, 180, 280, 380, window, true));
+    button.menuNumSet(0);
+}
+
 }
